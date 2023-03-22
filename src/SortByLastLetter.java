483675dee0682399c03da747785dcd29e4fcd959
konOkoz <new2pro.ware@gmail.com>
2023-03-22 12:42:46 +0100

    second commit

--- src/SortByLastLetter.java
+++ src/SortByLastLetter.java
@@ -3,8 +3,8 @@ import java.util.Comparator;
 public class SortByLastLetter implements Comparator<Animal> {
 
     public int compare(Animal o1, Animal o2){
-        String length1 = o1.name.substring(o1.name.length()-1);
-        String length2 = o2.name.substring(o2.name.length()-1);
-        return length1.compareTo(length2);
+        String letter1 = o1.name.substring(o1.name.length()-1);
+        String letter2 = o2.name.substring(o2.name.length()-1);
+        return letter1.compareTo(letter2);
     }
 }
